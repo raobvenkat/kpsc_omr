553b32c363c4fbabaa42a113911f886bb8c288af
raobvenkat <raobvenkat@gmail.com>
2026-06-28 16:36:22 +0530

    test commit

--- hi.py
+++ hi.py
@@ -4,3 +4,6 @@ import easyocr
 # 1. Verify PyTorch sees the hardware
 print(f"PyTorch CUDA Status: {torch.cuda.is_available()}")
 print(f"Targeting Hardware: {torch.cuda.get_device_name(0)}")
+
+
+//test
